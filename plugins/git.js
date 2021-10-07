f5cd6ee481c77ad75163969f6124df77778faa8d
manaf-1 <83355105+manaf-1@users.noreply.github.com>
2021-10-07 15:30:46 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,7 +11,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.GL}, (async (message
     
     
    
-  r_text[0] = "https://i.imgur.com/xxxtentacion.jpg";
+  r_text[0] = "https://i.imgur.com/BVLUrXJ.jpeg";
     
     
     var i = Math.floor(1*Math.random())
